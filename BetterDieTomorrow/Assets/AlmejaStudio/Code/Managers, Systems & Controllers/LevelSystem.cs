e1dd49455f5577a587ec364ed53bcb2fc65fbde2
Angel Legorreta <cdmx1895@amerike.edu.mx>
2024-05-04 23:36:44 -0500

    First Menu

---
+++ BetterDieTomorrow/Assets/AlmejaStudio/Code/Managers, Systems & Controllers/LevelSystem.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSystem : MonoBehaviour
+{
+    private static LevelSystem _instance;
+    public static  LevelSystem Instance
+    {
+        get { return _instance; }
+    }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            Debug.LogWarning("Mas controladores de tipo Scenetransition se han encontrado en la escena");
+            return;
+        }
+
+        _instance = this;
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+    
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+    
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
